a71e8300a4d8cf0230d1edc1a98d5a9942248aa3
o.naumets <o.naumets@doc.ua>
2020-12-15 20:56:34 +0200

    add component types

--- app/entity/AddressComponents.go
+++ app/entity/AddressComponents.go
@@ -1,16 +1,24 @@
 package entity
 
 import (
-	"time"
 	"github.com/jackc/pgtype"
+	"gorm.io/gorm"
+	"time"
 )
 
 type AddressComponents struct {
 	Id int32 `json:"id"`
-	Types pgtype.VarcharArray `gorm:"type:character varying[];" json:"types"`
-	LongName string `json:"long_name"`
+	Types pgtype.VarcharArray `gorm:"type:character varying[];" json:"-"`
+	LongName string `json:"name"`
 	ShortName string `json:"-"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
+
+	TypeData []pgtype.Varchar `gorm:"-" sql:"-" json:"types"`
+}
+
+func (c *AddressComponents) AfterFind(tx *gorm.DB) (err error) {
+	c.TypeData = c.Types.Elements
+	return
 }
 
